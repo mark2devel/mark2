f706d2226302ee85ed608b96ec80d6689fbd62a2
Ed Kellett <edk141@gmail.com>
2012-11-19 01:32:18 +0000

    Register ~su or ~raw depending on configuration
    ~su is registered if commands do not expand to 'sudo <anything>'
    ~raw is registered if commands expand to anything at all

--- plugins/su.py
+++ plugins/su.py
@@ -1,15 +1,19 @@
-import os
-import re
-
 from plugins import Plugin, Command
 
-class SU(Plugin):
-    command  = "sudo -su {user} -- {command}"
+
+class Su(Plugin):
+    command = "sudo -su {user} -- {command}"
     
     def setup(self):
-        self.register(Command(self.su, "su", "run a command from your username, e.g. ~su ban Notch"))
+        # register ~raw if commands are modified in any way,
+        # register ~su if commands do not get 'sudo ' at the beginning
+        if self.parent.expand_command('', '') != '':
+            self.register(Command(self.raw, "raw", "send a command unmodified to the server"))
+        if not self.parent.expand_command('', '').lower().startswith('sudo '):
+            self.register(Command(self.su, "su", "run a command from your username, e.g. ~su ban Notch"))
     
     def su(self, user, line):
         self.send(self.command.format(user=user, command=line))
-            
-ref = SU
+    
+    def raw(self, user, line):
+        self.send(line)
