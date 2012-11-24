eefef913ae3878701722e6399537fcce7ff37c5e
Barney Gale <barney.gale@gmail.com>
2012-11-24 04:43:15 +0000

    (hopefully) fix kicking banned players in mcbouncer plugin.

--- plugins/mcbouncer.py
+++ plugins/mcbouncer.py
@@ -41,14 +41,14 @@ class MCBouncer(Plugin):
     
     @register(Interest, 'INFO', '([A-Za-z0-9_]{1,16})\[/([0-9\.]+):\d+\] logged in with entity id .+')
     def on_login(self, match):
-        self.bouncer.getBanReason(match.group(1), callback=self.ban_reason)
+        self.bouncer.getBanReason(match.group(1), callback=lambda d: self.ban_reason(m.group(1), d))
         if not self.proxy_mode:
             self.bouncer.updateUser(match.group(1), match.group(2))
             self.bouncer.getIPBanReason(match.group(2), callback=lambda d: self.ip_ban_reason(m.group(1), d))
     
-    def ban_reason(self, details):
+    def ban_reason(self, user, details):
         if details['is_banned']:
-            self.send('kick %s Banned: %s' % (details['user'], details['reason']))
+            self.send('kick %s Banned: %s' % (user, details['reason']))
     
     def ip_ban_reason(self, user, details):
         if details['is_banned']:
