43a83c42bca30881d9f26dedbfcc5f71b6d8d389
Barney Gale <barney.gale@gmail.com>
2012-12-08 19:12:13 +0000

    make su functionality simpler and clearer

--- plugins/su.py
+++ plugins/su.py
@@ -2,23 +2,21 @@ from plugins import Plugin
 from events import UserInput
 import re
 
-modes = ('raw', 'su')
-
 class Su(Plugin):
     command = "sudo -su {user} -- {command}"
-    default = "raw"
-    exceptions = ""
+    mode = "include"
+    proc = "ban;unban"
     
     def setup(self):
         self.exceptions = re.split("\s*[\;\,]\s*", self.exceptions)
         self.register(self.uinput, UserInput)
     
     def uinput(self, event):
-        is_raw = (self.default == modes[1]) ^ (event.line in self.exceptions)
-        if is_raw:
-            self.send(event.line)
-        else:
-            self.send(self.command.format(user=event.user, command=event.line))
+        handled = False
+        for p in self.proc.split(";"):
+            if event.line.startswith(p):
+                handled = True
+                break
         
-        event.handled   = True
-        event.cancelled = True
+        if (self.mode == 'exclude') ^ handled:
+            event.line = self.command.format(user=event.user, command=event.line)
