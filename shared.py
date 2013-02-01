69e0dd620efe84bda2674f471e344a527eee3ba3
Barney Gale <barney.gale@gmail.com>
2013-02-01 22:52:31 +0000

    shared: fix unicode representation of a Console event

--- shared.py
+++ shared.py
@@ -1,9 +1,9 @@
 def console_repr(e):
-    s = "%s %s " % (e.time, {'server': '|', 'mark2': '#', 'user': '>'}.get(e.source, '?'))
+    s = u"%s %s " % (e.time, {'server': '|', 'mark2': '#', 'user': '>'}.get(e.source, '?'))
     if e.source == 'server' and e.level != 'INFO':
-        s += "[%s] " % e.level
+        s += u"[%s] " % e.level
     elif e.source == 'user':
-        s += "(%s) " % e.user
+        s += u"(%s) " % e.user
     
-    s += "%s" % e.data
+    s += u"%s" % e.data
     return s
