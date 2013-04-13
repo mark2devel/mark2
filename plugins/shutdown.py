f1cee9e28307de19fb1b80fa5dfceffadfffa000
Ed Kellett <edk141@gmail.com>
2013-04-13 14:14:37 +0100

    plugins/shutdown: add ~cancel to prevent the next restart or shutdown

--- plugins/shutdown.py
+++ plugins/shutdown.py
@@ -3,17 +3,24 @@ from events import Hook, ServerStop, StatPlayers
 
 
 class Shutdown(Plugin):
-    restart_warn_message = "WARNING: planned restart in {delay}."
-    stop_warn_message    = "WARNING: server going down for planned maintainence in {delay}."
-    restart_message      = "Server restarting."
-    stop_message         = "Server going down for maintainence."
-    kick_command         = "kick {player} {message}"
-    kick_mode            = "all"
+    restart_warn_message   = "WARNING: planned restart in {delay}."
+    stop_warn_message      = "WARNING: server going down for planned maintainence in {delay}."
+    restart_message        = "Server restarting."
+    stop_message           = "Server going down for maintainence."
+    restart_cancel_message = "WARNING: planned restart cancelled."
+    restart_cancel_reason  = "WARNING: planned restart cancelled ({reason})."
+    stop_cancel_message    = "WARNING: planned maintenance cancelled."
+    stop_cancel_reason     = "WARNING: planned maintenance cancelled ({reason})."
+    kick_command           = "kick {player} {message}"
+    kick_mode              = "all"
     
     failsafe = None
+
+    cancel_preempt = 0
     
     def setup(self):
         self.players = []
+        self.cancel = []
         
         self.register(self.handle_players, StatPlayers)
         
@@ -21,12 +28,20 @@ class Shutdown(Plugin):
         self.register(self.h_restart,       Hook, public=True, name="restart",      doc='cleanly restart the server. specify a delay like `~restart 30s`')
         self.register(self.h_kill,          Hook, public=True, name="kill",         doc='kill the server')
         self.register(self.h_kill_restart,  Hook, public=True, name="kill-restart", doc='kill the server and bring it back up')
+        self.register(self.h_cancel,        Hook, public=True, name="cancel",       doc='cancel an upcoming shutdown or restart')
     
     def warn_restart(self, delay):
         self.send_format("say %s" % self.restart_warn_message, parseColors=True, delay=delay)
     
     def warn_stop(self, delay):
         self.send_format("say %s" % self.stop_warn_message, parseColors=True, delay=delay)
+
+    def warn_cancel(self, reason, thing):
+        if reason:
+            message = self.restart_cancel_reason if thing == "restart" else self.stop_cancel_reason
+        else:
+            message = self.restart_cancel_message if thing == "restart" else self.stop_cancel_message
+        self.send_format("say %s" % message, parseColors=True, reason=reason)
 
     def nice_stop(self, respawn, kill):
         if not kill:
@@ -41,19 +56,37 @@ class Shutdown(Plugin):
 
     def handle_players(self, event):
         self.players = event.players
+
+    def cancel_something(self, reason=None):
+        thing, cancel = self.cancel.pop(0)
+        cancel(reason, thing)
+
+    def should_cancel(self):
+        if self.cancel_preempt:
+            self.cancel_preempt -= 1
+            return True
+        else:
+            return False
     
     #Hook handlers:
     def h_stop(self, event=None):
+        if self.should_cancel():
+            self.console("I'm not stopping because this shutdown was cancelled with ~cancel")
+            return
         action = lambda: self.nice_stop(False, False)
         if event and event.args:
-            warn_length, action = self.action_chain(event.args, self.warn_stop, action)
+            warn_length, action, cancel = self.action_chain_cancellable(event.args, self.warn_stop, action, self.warn_cancel)
+            self.cancel.append(("stop", cancel))
         action()
-    
 
     def h_restart(self, event=None):
+        if self.should_cancel():
+            self.console("I'm not restarting because this shutdown was cancelled with ~cancel")
+            return
         action = lambda: self.nice_stop(True, False)
         if event and event.args:
-            warn_length, action = self.action_chain(event.args, self.warn_restart, action)
+            warn_length, action, cancel = self.action_chain_cancellable(event.args, self.warn_restart, action, self.warn_cancel)
+            self.cancel.append(("restart", cancel))
         action()
     
     def h_kill(self, event):
@@ -61,3 +94,10 @@ class Shutdown(Plugin):
     
     def h_kill_restart(self, event):
         self.nice_stop(True, True)
+
+    def h_cancel(self, event):
+        if self.cancel:
+            self.cancel_something(event.args or None)
+        else:
+            self.cancel_preempt += 1
+            self.console("I will cancel the next thing")
