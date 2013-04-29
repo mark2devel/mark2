8d28ddf87d7f8c025fb3e4d3de135a0cd11bb32f
barneygale <barney.gale@gmail.com>
2013-04-29 15:55:05 +0100

    Jar downloader: use mojang's new launcher repo

--- servers/vanilla.py
+++ servers/vanilla.py
@@ -1,32 +1,15 @@
-from xml.dom import minidom
+import json
 
 from servers import JarProvider
 
 class Vanilla(JarProvider):
-    base = 'http://assets.minecraft.net/'
-    target = '/minecraft_server.jar'
+    base = 'http://s3.amazonaws.com/Minecraft.Download/versions/'
     def work(self):
-        self.add(('Vanilla', 'Stable'), (None, None), 'https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar')
-        self.get(self.base, self.handle_latest)
+        self.get(self.base + 'versions.json', self.handle_data)
 
-    def handle_latest(self, data):
-        child = lambda n, name: n.getElementsByTagName(name)[0].childNodes[0].data
-
-        dom = minidom.parseString(data)
-
-        c_date = 0
-        c_path = None
-        for node in dom.getElementsByTagName("Contents"):
-            path = child(node, 'Key')
-            if path.endswith(self.target):
-                date = child(node, 'LastModified')
-                if date > c_date:
-                    c_date = date
-                    c_path = path
-
-        if not c_path is None:
-            self.add(('Vanilla', 'Latest'), (None, None), self.base + c_path)
-
+    def handle_data(self, data):
+        for k, v in json.loads(data)['latest'].iteritems():
+            self.add(('Vanilla', k.title()), (None, None), '{0}{1}/minecraft_server.{1}.jar'.format(self.base, v))
         self.commit()
 
 ref = Vanilla
