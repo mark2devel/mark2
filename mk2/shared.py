cf7a2d4a0c461553ae5aaf7bd5ac989bb2f31dff
Ed Kellett <edk141@gmail.com>
2013-05-24 16:42:58 +0100

    shared: add helpers for finding resources and config files

--- mk2/shared.py
+++ mk2/shared.py
@@ -1,3 +1,47 @@
+import os
+import pkg_resources
+
+
+_requirement = pkg_resources.Requirement.parse("mark2")
+
+
+def open_resource(name):
+    return pkg_resources.resource_stream(_requirement, name)
+
+
+_config_base = None
+
+
+_config_try = [os.path.join(os.path.expanduser("~"), ".config", "mark2"), "/etc/mark2"]
+if "VIRTUAL_ENV" in os.environ:
+    _config_try.insert(0, os.path.join(os.environ["VIRTUAL_ENV"], ".config", "mark2"))
+
+
+def find_config(name):
+    global _config_base
+    if not _config_base:
+        for path in _config_try:
+            if os.path.exists(path):
+                _config_base = path
+                print "use {0}".format(path)
+                break
+
+    if not _config_base:
+        for path in _config_try:
+            try:
+                os.makedirs(path)
+                _config_base = path
+                print "use {0}".format(path)
+                break
+            except OSError:
+                pass
+
+    if not _config_base:
+        raise ValueError
+
+    return os.path.join(_config_base, name)
+
+
 def console_repr(e):
     s = u"%s %s " % (e['time'], {'server': '|', 'mark2': '#', 'user': '>'}.get(e['source'], '?'))
     if e['source'] == 'server' and e['level'] != 'INFO':
