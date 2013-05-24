06aa6e2bc6c6948d88d0ba3a550dc147fe81d656
Ed Kellett <edk141@gmail.com>
2013-05-24 23:28:48 +0100

    user_client: use python 2.6-compatible set syntax

--- mk2/user_client.py
+++ mk2/user_client.py
@@ -412,7 +412,7 @@ class LineFilter:
 
     def setdefault(self, action):
         if len(self._actions) == 0:
-            self._default = list({self.HIDE, self.SHOW} - {action})[0]
+            self._default = list(set(self.HIDE, self.SHOW) - set(action))[0]
 
     def apply(self, msg):
         current = self._default
