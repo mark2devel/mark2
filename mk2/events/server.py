6507b2761f1f2e6dddb7c507d6e3706f9652d82c
barneygale <barney.gale@gmail.com>
2013-12-14 00:18:37 +0000

    Console support for 1.6 and 1.7, vanilla and bukkit (+descendents)

--- mk2/events/server.py
+++ mk2/events/server.py
@@ -4,7 +4,7 @@ from . import Event, get_timestamp
 
 # input/output
 output_exp = re.compile(
-        r'^\[(\d{2}\:\d{2}\:\d{2})\s([A-Z]*)\]\:\s(.*)')
+        r'^(?:\d{4}-\d{2}-\d{2} |)\[?(\d{2}:\d{2}:\d{2})\]? \[?(?:[^\]]+?/|)([A-Z]+)\]:? (.*)')
 
 class ServerInput(Event):
     """Send data to the server's stdin. In plugins, a shortcut
