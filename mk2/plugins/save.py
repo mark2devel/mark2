1310ad5b37e801040e725f58d3693c2145cd12f3
gsand <gsandowns@gmail.com>
2014-12-03 18:11:19 -0800

    Update mark2 save plugin
    Allow changing the save command and save warning command

--- mk2/plugins/save.py
+++ mk2/plugins/save.py
@@ -5,12 +5,14 @@ from mk2.events import Hook
 class Save(Plugin):
     warn_message = Plugin.Property(default="WARNING: saving map in {delay}.")
     message      = Plugin.Property(default="MAP IS SAVING.")
+	warn_command = Plugin.Property(default="say %s")
+	save_command = Plugin.Property(default="save-all")
     
     def setup(self):
         self.register(self.save, Hook, public=True, name='save', doc='save the map')
     
     def warn(self, delay):
-        self.send_format("say %s" % self.warn_message, delay=delay)
+        self.send_format(self.warn_command % self.warn_message, delay=delay)
     
     def save(self, event):
         action = self.save_real
@@ -21,6 +23,6 @@ class Save(Plugin):
 
     def save_real(self):
         if self.message:
-            self.send('say %s' % self.message)
-        self.send('save-all')
+            self.send(self.warn_command % self.message)
+        self.send(self.save_command)
     
