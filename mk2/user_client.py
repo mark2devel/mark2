291cac259b06c077d992a1130107197201c44a6c
Column01 <colinandress999@gmail.com>
2021-07-29 18:16:37 -0400

    Fix too many focused when terminal autoscrolls

--- mk2/user_client.py
+++ mk2/user_client.py
@@ -337,6 +337,10 @@ class UI:
             else:
                 offset = 0
                 for attr, attr_length in _old_attr:
+                    # Ensure that standout lines are set properly
+                    if isinstance(attr, urwid.AttrSpec):
+                        if attr.foreground == 'default,standout':
+                            attr.foreground = 'default'
                     text_to_apply = _text_val[offset : attr_length + offset]
                     offset += attr_length
                     final_text.append((attr, text_to_apply))
